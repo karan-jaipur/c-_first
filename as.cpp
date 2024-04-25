e8bc8edd1777ae901e25a197d458588586dc0900
karan-jaipur <153348641+karan-jaipur@users.noreply.github.com>
2024-04-25 13:56:31 +0530

    array_reverse

--- as.cpp
+++ as.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-  cout<<"karan";
+    int n;
+      cout<<"Enter the number of array: ";
+      cin >> n;
+
+      int a[n];
+      cout<<"Enter "<<n <<" integers: ";
+      for(int i=0;i<n;i++) {
+          cin >> a[i];
+      }
+
+      cout<<"Reversed:";
+      for(int i=n-1;i>=0;i--){
+          cout<<a[i]<<" ";
+      }
 }
